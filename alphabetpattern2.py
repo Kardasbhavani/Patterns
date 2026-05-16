40c25d2b0b87144e673f0997f4a64cae4a5bf397
Kardasbhavani <bhavanikardas@gmail.com>
2026-05-16 20:34:05 +0530

    Implement alphabet pattern printing
    Add a program to print an alphabet pattern based on user input.

---
+++ alphabetpattern2.py
@@ -0,0 +1,15 @@
+n=int(input())
+ch=65
+for i in range(1,n+1): #i=row
+    for j in range(n-i): #j=column
+        print(" ",end=" ")
+    for j in range(1,i+1):
+        print(chr(ch),end=" ")
+        ch=ch+1
+    print()
+#output:
+# 4
+#      A 
+#    B C 
+#  D E F 
+#G H I J 
